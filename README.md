3a29b7c78bb956372ae8004d0045cb11f2ccf442
Matheus Resende <75645023+matheusresende7@users.noreply.github.com>
2024-08-04 20:18:05 -0300

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python_projeto_final_1_rpa_automacao_pyautogui
- Projeto para realizar a análise de uma base de dados em Excel e enviar e-mails para os gestores
+ Projeto para realizar a análise de uma base de dados em Excel e reportar a análise em e-mails para os gestores
+
+ Nome: Python - RPA | Automação | PyAutoGUI
+
+ Descrição: Nesse projeto de RPA e Automação, foram utilizadas principalmente as bibliotecas PyAutoGUI e Pandas do Python como base para automatizar os fluxos do processo. Essa automação consiste em analisar uma base de dados de vendas em Excel, extrair a cotação do Dólar no navegador para gerar indicadores da empresa e reportar isso em um e-mail para os gestores. Todo esse processo é realizado automaticamente sem nenhuma interferência do usuário.
--- code/functions.py
+++ code/functions.py
@@ -1,12 +1,13 @@
 # Importando as bibliotecas
 import pyautogui as pag
 import pyperclip
+import pygetwindow as gw
 
 def escrever_texto(texto): # Função para escrever um texto
     pyperclip.copy(texto) # Copiando o texto passado
     pag.hotkey("ctrl", "v") # Colando o texto passado
 
-def conversao_real (valor): # Função para converter um valor para real
+def conversao_moeda (valor): # Função para converter um valor para real
     valor = f'{valor:,.2f}' # Formatando com divisor de milhares e separador de casas decimais
     valor = valor.replace(',', 'X').replace('.', ',').replace('X', '.') # Substituindo , por . e . por ,
     return valor
@@ -15,3 +16,8 @@ def conversao_numero (valor): # Função para converter um valor para real
     valor = f'{valor:,.0f}' # Formatando com divisor de milhares e separador de casas decimais
     valor = valor.replace(',', '.') # Substituindo , por . e . por ,
     return valor
+
+def abrir_programa (programa): # Função para abrir um programa
+    pag.press ('Win') # Apertando a tecla Windowns
+    pag.write (programa) # Escrevendo o nome do programa a ser aberto
+    pag.press ('Enter') # Apertando enter para executar o programa
